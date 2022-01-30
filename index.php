c6e546b79be19f85bb0464153fce61634ac96561
narayaitsolution <narayaitsolution@gmail.com>
2022-01-30 16:37:50 +0700

    login ok

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+require('system/myfunc.php');
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,13 +9,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>SAINTEK e-Office</title>
 
-  <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Ubuntu:300,400,400i,700&display=fallback">
-  <!-- Font Awesome -->
   <link rel="stylesheet" href="template/plugins/fontawesome-free/css/all.min.css">
-  <!-- icheck bootstrap -->
   <link rel="stylesheet" href="template/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
-  <!-- Theme style -->
   <link rel="stylesheet" href="template/dist/css/adminlte.min.css">
   <style>
     .login-page {
@@ -25,17 +24,31 @@
 
 <body class="hold-transition login-page">
   <div class="login-box">
-    <!-- /.login-logo -->
     <div class="card card-outline card-primary">
       <div class="card-header text-center">
         <a href="#" class="h1"><img src="system/saintek-logo.png" width="100%"></a>
       </div>
       <div class="card-body">
+        <?php
+        if (isset($_GET['pesan'])) {
+          if ($_GET['pesan'] == "antibot") {
+        ?>
+            <div class="alert alert-danger alert-dismissible fade show">
+              <strong>ERROR!</strong> perhitungan salah!!
+            </div>
+          <?php
+          } elseif ($_GET['pesan'] == "gagal") {
+          ?>
+            <div class="alert alert-danger alert-dismissible fade show">
+              <strong>ERROR!</strong> User ID / Password salah
+            </div>
+        <?php
+          }
+        } ?>
         <p class="login-box-msg h3"><b>SAINTEK</b> e-Office</p>
-
         <form action="auth.php" method="post">
           <div class="input-group mb-3">
-            <input type="text" class="form-control" placeholder="User ID">
+            <input type="text" class="form-control" placeholder="User ID" name="userid" required>
             <div class="input-group-append">
               <div class="input-group-text">
                 <span class="fas fa-user"></span>
@@ -43,28 +56,29 @@
             </div>
           </div>
           <div class="input-group mb-3">
-            <input type="password" class="form-control" placeholder="Password">
+            <input type="password" class="form-control" placeholder="Password" name="pass" required>
             <div class="input-group-append">
               <div class="input-group-text">
                 <span class="fas fa-key"></span>
               </div>
             </div>
           </div>
-          <div class="row">
-            <!--
-            <div class="col-5">
-              <div class="icheck-primary">
-                <input type="checkbox" id="remember">
-                <label for="remember">
-                  Ingat saya
-                </label>
+          <?php
+          $angka1 = rand(1, 5);
+          $angka2 = rand(1, 5);
+          $kunci = $angka1 + $angka2;
+          ?>
+          <div class="input-group mb-3">
+            <input type="number" placeholder="<?= huruf($angka1); ?> ditambah <?= huruf($angka2); ?> ?" class="form-control" name="antibot" required>
+            <div class="input-group-append">
+              <div class="input-group-text">
+                <span class="fas fa-question"></span>
               </div>
             </div>
-  -->
-            <div class="col">
-              <button type="submit" class="btn btn-primary btn-lg btn-block">Masuk</button>
-            </div>
           </div>
+          <hr>
+          <input type="hidden" name="kunci" value="<?= $kunci; ?>">
+          <button type="submit" class="btn btn-primary btn-lg btn-block">Masuk</button>
         </form>
         <hr>
         <div class="row">
@@ -76,17 +90,11 @@
           </div>
         </div>
       </div>
-      <!-- /.card-body -->
     </div>
-    <!-- /.card -->
   </div>
-  <!-- /.login-box -->
 
-  <!-- jQuery -->
   <script src="template/plugins/jquery/jquery.min.js"></script>
-  <!-- Bootstrap 4 -->
   <script src="template/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <!-- AdminLTE App -->
   <script src="template/dist/js/adminlte.min.js"></script>
 </body>
 
