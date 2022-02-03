0ffbb6a147486d9eed1f4d4382fa8d58e37b880f
narayaitsolution <narayaitsolution@gmail.com>
2022-02-03 20:52:55 +0700

    mahasiswa in progress

---
+++ mahasiswa/index.php
@@ -0,0 +1,299 @@
+<?php
+session_start();
+$user = $_SESSION['user'];
+$nip = $_SESSION['nip'];
+$nama = $_SESSION['nama'];
+$prodi = $_SESSION['prodi'];
+$hakakses = $_SESSION['hakakses'];
+$jabatan = $_SESSION['jabatan'];
+if ($_SESSION['hakakses'] != "mahasiswa") {
+    header("location:../deauth.php");
+}
+require('../system/dbconn.php');
+require('../system/myfunc.php');
+$no = 1;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>SAINTEK e-Office</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Ubuntu:300,400,400i,700&display=fallback">
+    <link rel="stylesheet" href="../template/plugins/fontawesome-free/css/all.min.css">
+    <link rel="stylesheet" href="../template/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+    <link rel="stylesheet" href="../template/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+    <link rel="stylesheet" href="../template/plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
+    <link rel="stylesheet" href="../template/dist/css/adminlte.min.css">
+    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+</head>
+
+<body class="hold-transition sidebar-mini text-sm">
+    <!-- Site wrapper -->
+    <div class="wrapper">
+        <!-- Navbar -->
+        <?php
+        require('navbar.php');
+        ?>
+        <!-- /.navbar -->
+
+        <!-- Main Sidebar Container -->
+        <?php
+        require('sidebar.php');
+        ?>
+
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+            <!-- Content Header (Page header) -->
+            <section class="content-header">
+                <div class="container-fluid">
+                    <div class="row mb-2">
+                        <div class="col-sm-6">
+                            <h1>Dashboard</h1>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <!-- tabel pengajuan pribadi -->
+            <section class="content">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="card card-primary">
+                                <div class="card-header">
+                                    <h3 class="card-title">Pengajuan Surat Pribadi</h3>
+                                    <div class="card-tools">
+                                        <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i></button>
+                                    </div>
+                                </div>
+                                <?php $no = 1; ?>
+                                <div class="card-body p-0">
+                                    <div class="card-body">
+                                        <table id="example2" class="table table-bordered table-hover text-sm">
+                                            <thead>
+                                                <tr>
+                                                    <th width="5%" style="text-align: center;">No</th>
+                                                    <th width="20%" style="text-align: center;">Jenis Surat</th>
+                                                    <th style="text-align: center;">Status Surat</th>
+                                                    <th width="20%" style="text-align: center;">Keterangan</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <!-- Izin-->
+                                                <?php
+                                                $query = mysqli_query($dbsurat, "SELECT * FROM izin WHERE nip='$nip' ORDER BY tglizin1 DESC");
+                                                while ($data = mysqli_fetch_array($query)) {
+                                                    $nodata = $data['no'];
+                                                    $jenissurat = 'Surat Izin';
+                                                    $keterangan = $data['keterangan'];
+                                                    $verifikasiprodi = $data['verifikasiprodi'];
+                                                    $verifikatorprodi = $data['verifikatorprodi'];
+
+                                                ?>
+                                                    <tr>
+                                                        <td><?= $no; ?></td>
+                                                        <td><?= $jenissurat; ?></td>
+                                                        <td>
+                                                            <?php
+                                                            if ($verifikasiprodi == 0) {
+                                                            ?>
+                                                                menunggu verifikasi <?= namadosen($dbsurat, $verifikatorprodi); ?>
+                                                            <?php
+                                                            };
+                                                            ?>
+                                                            <?php
+                                                            if ($verifikasiprodi == 1) {
+                                                            ?>
+                                                                <a class="btn btn-success btn-sm" href="izin-cetak.php?nodata=<?php echo $nodata; ?>" target="_blank">
+                                                                    <i class="fas fa-print"></i> Cetak
+                                                                </a>
+                                                            <?php
+                                                            };
+                                                            ?>
+                                                            <?php
+                                                            if ($verifikasiprodi == 2) {
+                                                            ?>
+                                                                Ditolak oleh <?= namadosen($dbsurat, $verifikatorprodi); ?>
+                                                            <?php
+                                                            };
+                                                            ?>
+                                                        </td>
+                                                        <td>
+                                                            <?= $keterangan; ?>
+                                                            <br />
+                                                            <?php
+                                                            if ($verifikasiprodi <> 1) {
+                                                            ?>
+                                                                <a class="btn btn-danger btn-sm" onclick="return confirm('Yakin menghapus pengajuan ini ?')" href="izin-hapus.php?nodata=<?php echo $nodata; ?>">
+                                                                    <i class="fas fa-trash"></i> Hapus
+                                                                </a>
+                                                            <?php
+                                                            };
+                                                            ?>
+                                                        </td>
+                                                    </tr>
+                                                <?php
+                                                    $no++;
+                                                }
+                                                ?>
+
+                                                <!-- pengajuan WFH-->
+                                                <?php
+                                                $query = mysqli_query($dbsurat, "SELECT * FROM wfh WHERE nip='$nip' ORDER BY tglwfh1 DESC");
+                                                while ($data = mysqli_fetch_array($query)) {
+                                                    $nodata = $data['no'];
+                                                    $tglwfh1 = $data['tglwfh1'];
+                                                    $tglwfh2 = $data['tglwfh2'];
+                                                    $tglwfh3 = $data['tglwfh3'];
+                                                    $tglwfh4 = $data['tglwfh4'];
+                                                    $tglwfh5 = $data['tglwfh5'];
+                                                    $verifikasiprodi = $data['verifikasiprodi'];
+                                                    $verifikatorprodi = $data['verifikatorprodi'];
+                                                    $verifikasifakultas = $data['verifikasifakultas'];
+                                                    $verifikatorfakultas = $data['verifikatorfakultas'];
+                                                    $jenissurat = 'Izin WFH';
+                                                    $keterangan = $data['keterangan'];
+                                                    if (date($tglwfh5) != 0) {
+                                                        $wfhselesai = $tglwfh5;
+                                                    } else {
+                                                        if (date($tglwfh4) != 0) {
+                                                            $wfhselesai = $tglwfh4;
+                                                        } else {
+                                                            if (date($tglwfh3) != 0) {
+                                                                $wfhselesai = $tglwfh3;
+                                                            } else {
+                                                                if (date($tglwfh2) != 0) {
+                                                                    $wfhselesai = $tglwfh2;
+                                                                }
+                                                            }
+                                                        }
+                                                    }
+                                                ?>
+                                                    <tr>
+                                                        <td><?= $no; ?></td>
+                                                        <td><?= $jenissurat; ?>
+                                                        </td>
+                                                        <td>
+                                                            <?php
+                                                            if ($verifikasiprodi == 0) {
+                                                            ?>
+                                                                menunggu verifikasi <?= namadosen($dbsurat, $verifikatorprodi); ?>
+                                                            <?php
+                                                            };
+                                                            ?>
+                                                            <?php
+                                                            if ($verifikasiprodi == 1) {
+                                                            ?>
+                                                                <a class="btn btn-success btn-sm" href="wfh-cetakrk.php?nodata=<?php echo $nodata; ?>" target="_blank">
+                                                                    <i class="fas fa-print"></i> Cetak Rencana Kerja
+                                                                </a>
+                                                            <?php
+                                                            };
+                                                            ?>
+                                                            <?php
+                                                            if ($verifikasiprodi == 2) {
+                                                            ?>
+                                                                Ditolak oleh <?= namadosen($dbsurat, $verifikatorprodi); ?>
+                                                            <?php
+                                                            };
+                                                            ?>
+
+                                                            <?php
+                                                            if ($verifikasifakultas == 0) {
+                                                            ?>
+                                                                Menunggu verifikasi <?= namadosen($dbsurat, $verifikatorfakultas); ?>
+                                                            <?php
+                                                            };
+                                                            ?>
+                                                            <?php
+                                                            if ($verifikasifakultas == 1) {
+                                                            ?>
+                                                                <a class="btn btn-success btn-sm" href="wfh-cetakst.php?nodata=<?php echo $nodata; ?>" target="_blank">
+                                                                    <i class="fas fa-print"></i> Cetak Surat Tugas
+                                                                </a>
+                                                            <?php
+                                                            };
+                                                            ?>
+                                                            <?php
+                                                            if ($verifikasifakultas == 2) {
+                                                            ?>
+                                                                Ditolak oleh <?= namadosen($dbsurat, $verifikatorfakultas); ?>
+                                                            <?php
+                                                            };
+                                                            ?>
+                                                        </td>
+                                                        <td>
+                                                            <?= $keterangan; ?>
+                                                            <br />
+                                                            <?php
+                                                            if ($verifikasifakultas <> 1) {
+                                                            ?>
+                                                                <a class="btn btn-danger btn-sm" onclick="return confirm('Yakin menghapus pengajuan ini ?')" href="wfh-hapus.php?nodata=<?php echo $nodata; ?>">
+                                                                    <i class="fas fa-trash"></i> Hapus
+                                                                </a>
+                                                            <?php
+                                                            };
+                                                            ?>
+                                                        </td>
+                                                    </tr>
+                                                <?php
+                                                    $no++;
+                                                }
+                                                ?>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+        </div>
+    </div>
+    <?php
+    require('footer.php');
+    ?>
+
+    <script src="../template/plugins/jquery/jquery.min.js"></script>
+    <script src="../template/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="../template/plugins/datatables/jquery.dataTables.min.js"></script>
+    <script src="../template/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+    <script src="../template/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+    <script src="../template/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+    <script src="../template/plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
+    <script src="../template/plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
+    <script src="../template/plugins/jszip/jszip.min.js"></script>
+    <script src="../template/plugins/pdfmake/pdfmake.min.js"></script>
+    <script src="../template/plugins/pdfmake/vfs_fonts.js"></script>
+    <script src="../template/plugins/datatables-buttons/js/buttons.html5.min.js"></script>
+    <script src="../template/plugins/datatables-buttons/js/buttons.print.min.js"></script>
+    <script src="../template/plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
+    <script src="../template/dist/js/adminlte.min.js"></script>
+
+    <script>
+        $(function() {
+            $("#example1").DataTable({
+                "responsive": true,
+                "lengthChange": false,
+                "autoWidth": false,
+                "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
+            }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+            $('#example2').DataTable({
+                "paging": true,
+                "lengthChange": false,
+                "searching": true,
+                "ordering": true,
+                "info": true,
+                "autoWidth": false,
+                "responsive": true,
+            });
+        });
+    </script>
+</body>
+
+</html>
---
+++ mahasiswa/sidebar.php
@@ -0,0 +1,46 @@
+<!--
+<style>
+    .main-sidebar {
+        background-color: clear !important
+    }
+</style>
+-->
+
+<aside class="main-sidebar sidebar-light-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="#" class="brand-link">
+        <img src="../system/uinlogo-sm.png" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <span class="brand-text font-weight-light"><b>SAINTEK e-Office</b></span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+        <!-- Sidebar user (optional) -->
+
+
+        <!-- Sidebar Menu -->
+        <nav class="mt-2">
+            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                <li class="nav-item">
+                    <a href="index.php" class="nav-link">
+                        <i class="nav-icon fas fa-tv"></i>
+                        <p>
+                            Dashboard
+                            <span class="right badge badge-danger"></span>
+                        </p>
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a href="mailto:narayaitsolution@gmail.com" class="nav-link">
+                        <i class="nav-icon fas fa-question-circle"></i>
+                        <p>
+                            Bantuan
+                            <span class="right badge badge-danger"></span>
+                        </p>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</aside>
