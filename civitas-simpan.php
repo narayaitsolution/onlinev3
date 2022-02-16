cc40f3f98f79d038d257bfc8beaff083ca4266ee
narayaitsolution <narayaitsolution@gmail.com>
2022-02-16 08:45:10 +0700

    fixing masuk fakultas

--- civitas-simpan.php
+++ civitas-simpan.php
@@ -7,7 +7,6 @@ $nim = $_SESSION['nip'];
 $nama = $_SESSION['nama'];
 $prodi = $_SESSION['prodi'];
 $hakakses = $_SESSION['hakakses'];
-$token = $_SESSION['token'];
 $suhu = mysqli_real_escape_string($dbsurat, $_POST['suhu']);
 if ($hakakses == 'mahasiswa') {
     $keperluan = $_POST['keperluan'];
@@ -19,9 +18,9 @@ if ($suhu >= 37.3) {
 } elseif ($suhu < 35) {
     header("location:civitas-masuk.php?pesan=rendah");
 } else {
-    $stmt = $dbsurat->prepare("INSERT INTO masukfakultas (tanggal, token, nim, nama, prodi, hakakses,suhu, keperluan, jammasuk)
-                                VALUES (?,?,?,?,?,?,?,?,?)");
-    $stmt->bind_param("sssssssss", $tanggal, $token, $nim, $nama, $prodi, $hakakses, $suhu, $keperluan, $tanggal);
+    $stmt = $dbsurat->prepare("INSERT INTO masukfakultas (tanggal, nim, nama, prodi, hakakses,suhu, keperluan, jammasuk)
+                                VALUES (?,?,?,?,?,?,?,?)");
+    $stmt->bind_param("ssssssss", $tanggal, $nim, $nama, $prodi, $hakakses, $suhu, $keperluan, $tanggal);
     $stmt->execute();
 
     header("location:civitas-tampil.php");
