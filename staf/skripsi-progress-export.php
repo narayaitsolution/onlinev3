9d1d81be45c5195aee9e21d1268807f8b65aeec4
narayaitsolution <narayaitsolution@gmail.com>
2023-04-02 20:48:12 +0700

    progress skripsi kasubag akademik in progress

--- staf/skripsi-progress-export.php
+++ staf/skripsi-progress-export.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
 require('../system/dbconn.php');
-require('../system/myfunc.php');
-require('../system/PhpSpreadsheet');
+//require('../system/myfunc.php');
+//require('../system/PhpSpreadsheet');
 $user = $_SESSION['user'];
 $nip = $_SESSION['nip'];
 $nama = $_SESSION['nama'];
@@ -21,13 +21,18 @@ $prodi = $_POST['prodi'];
 $tglawal = $_POST['tglawal'];
 $tglakhir = $_POST['tglakhir'];
 
-echo $prodi;
-echo $tglawal;
-echo $tglakhir;
+// Set the file name and headers
+$filename = 'progress-skripsi-mahasiswa.csv';
+header('Content-Type: application/csv');
+header('Content-Disposition: attachment; filename="' . $filename . '"');
 
-// Create a new spreadsheet object
-$spreadsheet = new \PhpOffice\PhpSpreadsheet\Spreadsheet();
-// Select the active worksheet
-$worksheet = $spreadsheet->getActiveSheet();
+// Open the output stream
+$output = fopen('php://output', 'w');
+// Write the column headers to the output stream
+fputcsv($output, array('Prodi', 'NIM', 'Pembimbing1', 'Pembimbing2', 'Penguji1', 'Penguji2', 'Tgl Ujian'));
 
-$qskripsi = mysqli_query($dbsurat, "SELECT * FROM skripsi WHERE tanggal BETWEEN '$tglawal' AND '$tglakhir'");
+$qskripsi = mysqli_query($dbsurat, "SELECT prodi,nim,pembimbing1,pembimbing2,penguji1,penguji2,tglujian FROM skripsi WHERE tglujian BETWEEN '$tglawal' AND '$tglakhir' ORDER BY nim");
+while ($dskripsi = mysqli_fetch_array($qskripsi)) {
+    fputcsv($output, $dskripsi);
+}
+fclose($output);
