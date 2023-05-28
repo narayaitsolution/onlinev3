2680a0bfc47b9493de00c021d24d449e03b4d3d3
Naraya IT Solution <narayaitsolution@gmail.com>
2023-05-29 05:16:08 +0700

    fixing penghargaan-wd-tolak.php

--- dosen/penghargaan-wd-tolak.php
+++ dosen/penghargaan-wd-tolak.php
@@ -4,6 +4,8 @@ require_once('../system/dbconn.php');
 $nip = $_SESSION['nip'];
 
 $token = mysqli_real_escape_string($dbsurat, $_POST['token']);
+$nodata = mysqli_real_escape_string($dbsurat, $_POST['nodata']);
+$nimketua = mysqli_real_escape_string($dbsurat, $_POST['nimmhs']);
 
 date_default_timezone_set("Asia/Jakarta");
 $tgl = date('Y-m-d H:i:s');
@@ -18,4 +20,8 @@ $sql = mysqli_query($dbsurat, "UPDATE penghargaan
                     statussurat=2
 					WHERE token = '$token'");
 
+//update status anggota kelompok
+$sql = mysqli_query($dbsurat, "UPDATE penghargaananggota
+					SET statussurat = 2
+					WHERE nodata='$nodata' AND nimketua='$nimketua'");
 header("location:index.php");
