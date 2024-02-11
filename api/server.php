93f08272f2b18fc1e8f3636e871b6aa2ebf83892
narayaitsolution <narayaitsolution@gmail.com>
2024-02-11 21:10:23 +0700

    add restapi server

---
+++ api/server.php
@@ -0,0 +1,23 @@
+<?php
+header("Content-Type:application/json");
+require('../system/dbconn.php');
+
+$userid = $_GET['userid'];
+$pass = $_GET['pass'];
+$aktif = 1;
+
+$stmt = $dbsurat->prepare("SELECT * FROM pengguna WHERE user=? AND pass=? AND aktif=?");
+$stmt->bind_param("sss", $userid, $pass, $aktif);
+$stmt->execute();
+$result = $stmt->get_result();
+$jhasil = $result->num_rows;
+if ($jhasil > 0) {
+    $dhasil = $result->fetch_assoc();
+    $nama = $dhasil['nama'];
+    $nip = $dhasil['nip'];
+    $nohp = $dhasil['nohp'];
+    $email = $dhasil['email'];
+    $prodi = $dhasil['prodi'];
+    $hakakses = $dhasil['hakakses'];
+    echo json_encode(array('nama' => $nama, 'nip' => $nip, 'nohp' => $nohp, 'email' => $email, 'prodi' => $prodi, 'hakakses' => $hakakses));
+}
