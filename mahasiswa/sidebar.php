b6b758ac163bc5a05a00db9f6437223717e52954
narayaitsolution <narayaitsolution@gmail.com>
2024-04-26 16:49:55 +0700

    fixing pengajuan delegasi hanya bisa 1x

--- mahasiswa/sidebar.php
+++ mahasiswa/sidebar.php
@@ -125,19 +125,15 @@
                             $dmenu = mysqli_fetch_array($qmenu);
                             $statussurat = $dmenu['status'];
                             if ($statussurat == 1) {
-                                $qpenghargaan = mysqli_query($dbsurat, "SELECT * FROM penghargaan WHERE nim='$nim'");
-                                $jpenghargaan = mysqli_fetch_array($qpenghargaan);
-                                if ($jpenghargaan == 0) {
                             ?>
-                                    <li class="nav-item">
-                                        <a href="penghargaan-isi.php" class="nav-link">
-                                            <i class="nav-icon fas fa-trophy"></i>
-                                            <p>Penghargaan Prestasi</p>
-                                            <span class="badge badge-danger right">Baru</span>
-                                        </a>
-                                    </li>
+                                <li class="nav-item">
+                                    <a href="penghargaan-isi.php" class="nav-link">
+                                        <i class="nav-icon fas fa-trophy"></i>
+                                        <p>Penghargaan Prestasi</p>
+                                        <span class="badge badge-danger right">Baru</span>
+                                    </a>
+                                </li>
                             <?php
-                                }
                             }
                             ?>
 
@@ -148,15 +144,19 @@
                             $dmenu = mysqli_fetch_array($qmenu);
                             $statussurat = $dmenu['status'];
                             if ($statussurat == 1) {
+                                $qdelegasi = mysqli_query($dbsurat, "SELECT * FROM delegasi WHERE nim='$nim'");
+                                $jdelegasi = mysqli_fetch_array($qdelegasi);
+                                if ($jdelegasi == 0) {
                             ?>
-                                <li class="nav-item">
-                                    <a href="delegasi-isi.php" class="nav-link">
-                                        <i class="nav-icon fas fa-plane-departure"></i>
-                                        <p>Pengajuan Delegasi</p>
-                                        <span class="badge badge-danger right">Baru</span>
-                                    </a>
-                                </li>
+                                    <li class="nav-item">
+                                        <a href="delegasi-isi.php" class="nav-link">
+                                            <i class="nav-icon fas fa-plane-departure"></i>
+                                            <p>Pengajuan Delegasi</p>
+                                            <span class="badge badge-danger right">Baru</span>
+                                        </a>
+                                    </li>
                             <?php
+                                }
                             }
                             ?>
 
