f2881ec10427b68187cbb01f3654d348cf15212c
Naraya IT Solution <narayaitsolution@gmail.com>
2024-04-26 07:52:48 +0700

    update db account

--- system/dbconn.php
+++ system/dbconn.php
@@ -1,6 +1,6 @@
 <?php
 //$dbsurat = mysqli_connect("narayaitsolution.web.id", "narayait_online", "N-bf2lXrt()6", "narayait_online");
-$dbsurat = mysqli_connect("localhost", "narayait_online", "N-bf2lXrt()6", "narayait_online");
+$dbsurat = mysqli_connect("localhost", "saintek_online", "N-bf2lXrt()6", "saintek_online");
 // Check connection
 if (mysqli_connect_errno()) {
 	echo "Failed to connect to MySQL: " . mysqli_connect_error();
