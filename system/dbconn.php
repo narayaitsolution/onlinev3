94b8e7122948eda7d4c4ad3ece5677408039d6e1
narayaitsolution <narayaitsolution@gmail.com>
2024-06-05 05:28:20 +0700

    change db

--- system/dbconn.php
+++ system/dbconn.php
@@ -1,6 +1,7 @@
 <?php
 //$dbsurat = mysqli_connect("narayaitsolution.web.id", "narayait_online", "N-bf2lXrt()6", "narayait_online");
-$dbsurat = mysqli_connect("localhost", "saintek_online", "N-bf2lXrt()6", "saintek_online");
+//$dbsurat = mysqli_connect("localhost", "saintek_online", "N-bf2lXrt()6", "saintek_online");
+$dbsurat = mysqli_connect("hosting.uin-malang.ac.id", "saintek_online-dev", "qh1!R99t6", "saintek_online-dev");
 // Check connection
 if (mysqli_connect_errno()) {
 	echo "Failed to connect to MySQL: " . mysqli_connect_error();
