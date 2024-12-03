934fbfa76b966934ba1f8b990e83649ab96ad1d2
narayaitsolution <narayaitsolution@gmail.com>
2024-12-03 19:02:17 +0700

    intiial setip again

--- system/dbconn.php
+++ system/dbconn.php
@@ -1,6 +1,6 @@
 <?php
 //$dbsurat = mysqli_connect("narayaitsolution.web.id", "narayait_online", "N-bf2lXrt()6", "narayait_online");
-$dbsurat = mysqli_connect("localhost", "saintek_online", "N-bf2lXrt()6", "saintek_online");
+$dbsurat = mysqli_connect("localhost", "onlinev3", "rahasia", "onlinev3");
 //$dbsurat = mysqli_connect("hosting.uin-malang.ac.id", "saintek_online-dev", "qh1!R99t6", "saintek_online-dev");
 // Check connection
 if (mysqli_connect_errno()) {
